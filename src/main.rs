b01a9ab8810bb612935a3aaec3eaeb5927c28c96
Hiromitsu Oshiba <h.oshiba.0117@gmail.com>
2018-05-12 22:13:17 +0900

    fix: analyzing flow about zip file

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ use signatures::*;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "kick")]
 struct Options {
-
   /// entrypoint (used when target is dll)
   #[structopt(short = "e", long = "entry", default_value = "#1")]
   entrypoint: String,
@@ -24,7 +23,6 @@ struct Options {
   /// execution target
   #[structopt(name = "FILE")]
   file: String,
-
 }
 
 fn copy_with_ext(p: &str, ext: &str) -> std::io::Result<String> {
@@ -34,8 +32,7 @@ fn copy_with_ext(p: &str, ext: &str) -> std::io::Result<String> {
 }
 
 fn kick(opt: &Options) -> std::io::Result<()> {
-
-  match detect_filetype(opt.file.as_str())? {
+  match detect_filetype(&opt.file)? {
     // exe file
     FileType::Exe => {
       println!("deal as exe");
@@ -45,7 +42,7 @@ fn kick(opt: &Options) -> std::io::Result<()> {
         .arg("start")
         .arg(dest.as_str())
         .spawn()?;
-    },
+    }
 
     // dll file
     FileType::Dll => {
@@ -56,7 +53,7 @@ fn kick(opt: &Options) -> std::io::Result<()> {
         .arg("rundll32.exe")
         .arg(format!("{},{}", dest.as_str(), opt.entrypoint.as_str()))
         .spawn()?;
-    },
+    }
 
     // doc file
     FileType::Doc => {
@@ -100,50 +97,41 @@ fn kick(opt: &Options) -> std::io::Result<()> {
         .arg("-jar")
         .arg(dest.as_str())
         .spawn()?;
-    },
+    }
+
+    FileType::Docx => {
+      println!("this is docx!");
+      let dest = copy_with_ext(opt.file.as_str(), "docx")?;
+      Command::new("cmd")
+        .arg("/c")
+        .arg("start")
+        .arg(dest.as_str())
+        .spawn()?;
+    }
+
+    FileType::Xlsx => {
+      println!("this is xlsx!");
+      let dest = copy_with_ext(opt.file.as_str(), "xlsx")?;
+      Command::new("cmd")
+        .arg("/c")
+        .arg("start")
+        .arg(dest.as_str())
+        .spawn()?;
+    }
+
+    FileType::Pptx => {
+      println!("this is pptx!");
+      let dest = copy_with_ext(opt.file.as_str(), "pptx")?;
+      Command::new("cmd")
+        .arg("/c")
+        .arg("start")
+        .arg(dest.as_str())
+        .spawn()?;
+    }
 
     FileType::Zip => {
       println!("deal as zip");
-      println!("analyzing detail..");
-
-      match analze_zip(opt.file.as_str())? {
-
-        FileType::Docx => {
-          println!("this is docx!");
-          let dest = copy_with_ext(opt.file.as_str(), "docx")?;
-          Command::new("cmd")
-            .arg("/c")
-            .arg("start")
-            .arg(dest.as_str())
-            .spawn()?;
-        },
-
-        FileType::Xlsx => {
-          println!("this is xlsx!");
-          let dest = copy_with_ext(opt.file.as_str(), "xlsx")?;
-          Command::new("cmd")
-            .arg("/c")
-            .arg("start")
-            .arg(dest.as_str())
-            .spawn()?;
-        },
-
-        FileType::Pptx => {
-          println!("this is pptx!");
-          let dest = copy_with_ext(opt.file.as_str(), "pptx")?;
-          Command::new("cmd")
-            .arg("/c")
-            .arg("start")
-            .arg(dest.as_str())
-            .spawn()?;
-        },
-
-        _ => {
-          println!("other zip archive..");
-        },
-
-      }
-
+      println!("cannot exec this..");
     }
 
     // other
@@ -154,7 +142,7 @@ fn kick(opt: &Options) -> std::io::Result<()> {
         .arg("start")
         .arg(opt.file.as_str())
         .spawn()?;
-    },
+    }
   }
 
   Ok(())
@@ -167,5 +155,4 @@ fn main() {
     Ok(_) => println!("kicked!"),
     Err(e) => panic!(e),
   }
-
 }
--- src/signatures/mod.rs
+++ src/signatures/mod.rs
@@ -1,10 +1,10 @@
 extern crate zip;
 
-use std::io::*;
 use std::fs::*;
+use std::io::*;
 
-use bytereader::*;
 use self::zip::ZipArchive;
+use bytereader::*;
 
 pub enum FileType {
   Exe,
@@ -21,7 +21,6 @@ pub enum FileType {
 }
 
 pub fn is_pe<T: Seek + Read>(buffer: &mut ByteReader<T>) -> Result<bool> {
-
   let bmagic = &mut [0; 2];
   buffer.take_raw_bytes_offset(bmagic, 0)?;
 
@@ -31,19 +30,16 @@ pub fn is_pe<T: Seek + Read>(buffer: &mut ByteReader<T>) -> Result<bool> {
   }
 
   // check pe signature
-  let bsig = &mut[0; 4];
+  let bsig = &mut [0; 4];
 
   let ntheader_offset = buffer.read_as_dword_offset(0x3c)? as u64;
   buffer.take_raw_bytes_offset(bsig, ntheader_offset)?;
 
   // has PE signatue?
   Ok(*bsig == [0x50, 0x45, 0x00, 0x00])
-
 }
 
-
 fn is_dll<T: Seek + Read>(buffer: &mut ByteReader<T>) -> Result<bool> {
-
   // e_lfanew -> DWORD offset of IMAGE_NT_HEADER @ offset 0x3c from file head
   let ntheader_offset = buffer.read_as_dword_offset(0x3c)? as u64;
 
@@ -55,25 +51,24 @@ fn is_dll<T: Seek + Read>(buffer: &mut ByteReader<T>) -> Result<bool> {
 
   // dll file has 0x2000 flag
   Ok(chr & 0x2000 != 0)
-
 }
 
 fn is_zip_archive<T: Seek + Read>(buffer: &mut ByteReader<T>) -> Result<bool> {
-  let bmagic = &mut[0; 2];
+  let bmagic = &mut [0; 2];
   buffer.take_raw_bytes_offset(bmagic, 0)?;
 
   Ok(*bmagic == [0x50, 0x4b])
 }
 
 fn is_jar<T: Seek + Read>(buffer: &mut ByteReader<T>) -> Result<bool> {
-  let bmagic = &mut[0; 10];
+  let bmagic = &mut [0; 10];
   buffer.take_raw_bytes_offset(bmagic, 0)?;
 
   Ok(*bmagic == [0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x08, 0x00, 0x08, 0x00])
 }
 
 fn is_ole2<T: Seek + Read>(buffer: &mut ByteReader<T>) -> Result<bool> {
-  let bmagic = &mut[0; 8];
+  let bmagic = &mut [0; 8];
   buffer.take_raw_bytes_offset(bmagic, 0)?;
 
   Ok(*bmagic == [0xd0, 0xcf, 0x11, 0xe0, 0xa1, 0xb1, 0x1a, 0xe1])
@@ -85,20 +80,23 @@ fn is_doc<T: Seek + Read>(buffer: &mut ByteReader<T>) -> Result<bool> {
 }
 
 fn is_xls<T: Seek + Read>(buffer: &mut ByteReader<T>) -> Result<bool> {
-
   let bytes = buffer.read_as_word_offset(30)? as u64;
   let bpos = buffer.read_as_dword_offset(48)? as u64;
 
   let baddr = 512 + bpos * (1 << bytes) + 128;
 
   let b_pattern1 = &mut [0; 16];
-  buffer.take_raw_bytes_offset(b_pattern1, baddr);
-  if *b_pattern1 == [0x57, 0x00, 0x6f, 0x00, 0x72, 0x00, 0x6b, 0x00, 0x62, 0x00, 0x6f, 0x00, 0x6f, 0x00, 0x6b, 0x00] {
+  buffer.take_raw_bytes_offset(b_pattern1, baddr)?;
+  if *b_pattern1
+    == [
+      0x57, 0x00, 0x6f, 0x00, 0x72, 0x00, 0x6b, 0x00, 0x62, 0x00, 0x6f, 0x00, 0x6f, 0x00, 0x6b,
+      0x00,
+    ] {
     return Ok(true);
   }
 
   let b_pattern2 = &mut [0; 8];
-  buffer.take_raw_bytes_offset(b_pattern2, baddr);
+  buffer.take_raw_bytes_offset(b_pattern2, baddr)?;
   if *b_pattern2 == [0x42, 0x00, 0x6f, 0x00, 0x6f, 0x00, 0x6b, 0x00] {
     return Ok(true);
   }
@@ -113,46 +111,47 @@ fn is_ppt<T: Seek + Read>(buffer: &mut ByteReader<T>) -> Result<bool> {
   let baddr = 512 + bpos * (1 << bytes) + 128;
 
   let b_pattern1 = &mut [0; 19];
-  buffer.take_raw_bytes_offset(b_pattern1, baddr);
-  if *b_pattern1 == [
-    0x50, 0x00, 0x6f, 0x00, 0x77, 0x00, 0x65, 0x00, 0x72, 0x00,
-    0x50, 0x00, 0x6f, 0x00, 0x69, 0x00, 0x6e, 0x00, 0x74] {
-
-    buffer.take_raw_bytes_offset(b_pattern1, baddr + 19);
-    if *b_pattern1 == [
-      0x00, 0x20, 0x00, 0x44, 0x00, 0x6f, 0x00, 0x63, 0x00, 0x75,
-      0x00, 0x6d, 0x00, 0x65, 0x00, 0x6e, 0x00, 0x74, 0x00] {
+  buffer.take_raw_bytes_offset(b_pattern1, baddr)?;
+  if *b_pattern1
+    == [
+      0x50, 0x00, 0x6f, 0x00, 0x77, 0x00, 0x65, 0x00, 0x72, 0x00, 0x50, 0x00, 0x6f, 0x00, 0x69,
+      0x00, 0x6e, 0x00, 0x74,
+    ] {
+    buffer.take_raw_bytes_offset(b_pattern1, baddr + 19)?;
+    if *b_pattern1
+      == [
+        0x00, 0x20, 0x00, 0x44, 0x00, 0x6f, 0x00, 0x63, 0x00, 0x75, 0x00, 0x6d, 0x00, 0x65, 0x00,
+        0x6e, 0x00, 0x74, 0x00,
+      ] {
       return Ok(true);
     }
-
   }
-
 
   let b_pattern2 = &mut [0; 24];
-  buffer.take_raw_bytes_offset(b_pattern2, baddr);
-  if *b_pattern2 == [
-    0x43, 0x00, 0x75, 0x00, 0x72, 0x00, 0x72, 0x00, 0x65, 0x00,
-    0x6e, 0x00, 0x74, 0x00, 0x20, 0x00, 0x55, 0x00, 0x73, 0x00,
-    0x65, 0x00, 0x72, 0x00] {
+  buffer.take_raw_bytes_offset(b_pattern2, baddr)?;
+  if *b_pattern2
+    == [
+      0x43, 0x00, 0x75, 0x00, 0x72, 0x00, 0x72, 0x00, 0x65, 0x00, 0x6e, 0x00, 0x74, 0x00, 0x20,
+      0x00, 0x55, 0x00, 0x73, 0x00, 0x65, 0x00, 0x72, 0x00,
+    ] {
     return Ok(true);
   }
-
 
   let b_pattern3 = &mut [0; 16];
-  buffer.take_raw_bytes_offset(b_pattern3, baddr);
-  if *b_pattern3 == [
-    0x50, 0x00, 0x69, 0x00, 0x63, 0x00, 0x74, 0x00, 0x75, 0x00, 0x72, 0x00,
-    0x65, 0x00, 0x73, 0x00] {
+  buffer.take_raw_bytes_offset(b_pattern3, baddr)?;
+  if *b_pattern3
+    == [
+      0x50, 0x00, 0x69, 0x00, 0x63, 0x00, 0x74, 0x00, 0x75, 0x00, 0x72, 0x00, 0x65, 0x00, 0x73,
+      0x00,
+    ] {
     return Ok(true);
   }
-
 
   Ok(false)
 }
 
-pub fn analze_zip(t: &str) -> Result<FileType> {
-
-  let f = File::open(t)?;
+pub fn analyze_zip<S: AsRef<str>>(t: S) -> Result<FileType> {
+  let f = File::open(t.as_ref())?;
   let mut z = zip::ZipArchive::new(f)?;
 
   if let Ok(_) = z.by_name("word/document.xml") {
@@ -170,17 +169,18 @@ pub fn analze_zip(t: &str) -> Result<FileType> {
   return Ok(FileType::Zip);
 }
 
-pub fn detect_filetype(t: &str) -> Result<FileType> {
-  let f = File::open(t)?;
-  detect_type(f)
+pub fn detect_filetype<S: AsRef<str>>(t: &S) -> Result<FileType> {
+  let f = File::open(t.as_ref())?;
+  detect_type(f).and_then(|ft| match ft {
+    FileType::Zip => analyze_zip(t.as_ref()),
+    _ => Ok(ft),
+  })
 }
 
 pub fn detect_type<T: Seek + Read>(t: T) -> Result<FileType> {
-
   let mut buffer = ByteReader::from(t);
 
   if is_pe(&mut buffer)? {
-
     if is_dll(&mut buffer)? {
       return Ok(FileType::Dll);
     }
@@ -189,7 +189,6 @@ pub fn detect_type<T: Seek + Read>(t: T) -> Result<FileType> {
   }
 
   if is_ole2(&mut buffer)? {
-
     if is_doc(&mut buffer)? {
       return Ok(FileType::Doc);
     }
@@ -201,7 +200,6 @@ pub fn detect_type<T: Seek + Read>(t: T) -> Result<FileType> {
     if is_ppt(&mut buffer)? {
       return Ok(FileType::Ppt);
     }
-
   }
 
   if is_jar(&mut buffer)? {
@@ -213,5 +211,4 @@ pub fn detect_type<T: Seek + Read>(t: T) -> Result<FileType> {
   }
 
   Ok(FileType::Ohter)
-
 }
